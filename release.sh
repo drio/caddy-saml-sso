7d88d0a1e748717b2ae0cf3e503e5e0fc89d55f8
David Rio Deiros <driodeiros@gmail.com>
2023-03-03 08:32:09 -0500

    add release scripts

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ 
+set -e
+
+current=$(cat version.go | tail -1 | head -1 | awk -F= '{print $2}')
+echo "Current version is:"
+echo $current
+echo -ne "Enter new version: "
+read new_version
+
+
+(
+cat <<EOF
+package caddy_saml_sso
+
+const version = "$new_version"
+EOF
+) > version.go
+
+git add version.go
+git commit -m 'bump version'
+git tag "v$new_version"
+git push origin
+git push origin v$new_version
+
+rm -f caddy.*
+make release
+
+gh release create v$new_version
+gh release upload caddy.a*
